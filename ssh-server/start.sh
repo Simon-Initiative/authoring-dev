91370fc79bf5baa6ed4b0ba4694c8bb825ccd241
Raphael Gachuhi <rgachuhi@cmu.edu>
2018-03-07 11:00:30 -0500

    refactored editor base

--- ssh-server/start.sh
+++ ssh-server/start.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 __create_user() {
-# Create a user to SSH into as.
-useradd user
-SSH_USERPASS=$SSH_PASSWORD
-echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin user)
-echo ssh user password: $SSH_USERPASS
+    # Create a user to SSH into
+    user='user'
+    if [ $(getent passwd $user) ] ; then
+        echo user $user exists
+    else
+        useradd user
+        SSH_USERPASS=$SSH_PASSWORD
+        echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin user)
+        echo ssh user password: $SSH_USERPASS
+    fi
 }
 
 # Call all functions
--- wildfly-mysql/build.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/jjs -fv
-var cmd = "docker build -t oli/wildfly-mysql ."
-$EXEC(cmd);
-print($OUT);
-if ($ERR) print($ERR);
